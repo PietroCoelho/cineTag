d5e4f5c525ab47d0336d8d67097f46d52b8ce384
Pietro Coelho <coelhopietro17@gmail.com>
2024-08-19 22:33:28 -0300

    feat: create component card

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,5 +1,6 @@
 import Banner from "components/Banner";
 import Cabecalho from "components/Cabecalho";
+import Card from "components/Card";
 import Rodape from "components/Rodape";
 import Titulo from "components/Titulo";
 
@@ -11,6 +12,7 @@ const Inicio = () => {
             <Titulo>
                 <h1>Um lugar para guardar seus videos e filmes!</h1>
             </Titulo>
+            <Card id='1' titulo='hollow-knight' capa='https://www.deviantart.com/nessemdesigns/art/The-Knight-Hollow-Knight-949816396https://i.pinimg.com/originals/c0/2b/e8/c02be84a3ae18f1b69e71c45ce26033a.png' />
             <Rodape />
         </>
     )
