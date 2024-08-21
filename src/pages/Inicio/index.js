47dd0a899500ee7abe8775b79411bc200a82678b
Pietro Coelho <coelhopietro17@gmail.com>
2024-08-20 21:36:01 -0300

    feat: adding video elements cards

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -3,6 +3,8 @@ import Cabecalho from "components/Cabecalho";
 import Card from "components/Card";
 import Rodape from "components/Rodape";
 import Titulo from "components/Titulo";
+import styles from "./Inicio.module.css";
+import videos from "api/db.json";
 
 const Inicio = () => {
     return (
@@ -12,7 +14,11 @@ const Inicio = () => {
             <Titulo>
                 <h1>Um lugar para guardar seus videos e filmes!</h1>
             </Titulo>
-            <Card id='1' titulo='hollow-knight' capa='https://www.deviantart.com/nessemdesigns/art/The-Knight-Hollow-Knight-949816396https://i.pinimg.com/originals/c0/2b/e8/c02be84a3ae18f1b69e71c45ce26033a.png' />
+            <section className={styles.container}>
+                {videos.map((video) => {
+                    return <Card {...video} key={video.id} />
+                })}
+            </section>
             <Rodape />
         </>
     )
