05f462d0d5dfaa8483f75ebe5cf6eab1cb81ccfa
Pietro Coelho <coelhopietro17@gmail.com>
2024-09-02 20:12:03 -0300

    feat: adding route favorits

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,7 +1,20 @@
+import Banner from "components/Banner"
+import Titulo from "components/Titulo"
+import Card from "components/Card";
 import styles from "./Favoritos.module.css"
 
 const Favoritos = () => {
-    return <></>
+    return (
+        <>
+            <Banner image="favoritos" />
+            <Titulo>
+                <h1>Lista de favoritos</h1>
+            </Titulo>
+            <section className={styles.container}>
+                <Card id="2" titulo="teste" capa="https://thecatapi.com/api/images/get?format=src&type=png" />
+            </section>
+        </>
+    )
 }
 
 export default Favoritos
